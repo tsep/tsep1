d1ac62a95a4e6bc564f81b8add30e1289cd44638
geoffreyfishing <>
2010-11-21 23:10:37 +0000

    Completed queue system.

--- mvc/app/controllers/indexer_controller.php
+++ mvc/app/controllers/indexer_controller.php
@@ -16,16 +16,32 @@
 		var $name = 'Indexer';
 		var $uses = array('Index', 'Stopword', 'Element', 'Profile');
 		
-		function start($id = null) {
-
-			if ($id = null) $this->redirect(array('controller'=>'Indexer', 'action' => 'index'), null, true);
+		function admin_index ($id = null) {
+		
+		
+		}
+		
+		function admin_start($id = null) {
 			
-			$profile = $this->Profile->findById('profilename');
-			
-			if(empty($profile)) $this->redirect(array('controller'=>'Indexer', 'action' =>'index'), null, true);
+			$resume = false;
+			if (ctype_alnum($this->data['continue']))
+				if (file_exists(TMP.$this->data['continue']))
+					if (file_get_contents(TMP.$this->data['continue'])!= '')
+						$resume = true;
+			if (!$resume) {
+				if ($_SERVER['REMOTE_ADDR']!= $_SERVER['SERVER_ADDR']) $this->redirect(array('controller'=>'Indexer', 'action' => 'index', 'admin' => true), null, true);
+	
+				if ($id == null) $this->redirect(array('controller'=>'Indexer', 'action' => 'index','admin' => true), null, true);
+				
+				$profile = $this->Profile->findById($id);
+				
+				if(empty($profile)) $this->redirect(array('controller'=>'Indexer', 'action' =>'index', 'admin' => true), null, true);
+			}
+			else $profile = true;
 			
 			$this->_index($profile);
 		}
+		
 		
 		function _import()  {
 			
@@ -37,7 +53,6 @@
 			App::import('Vendor', 'random_string');
 			
 		}
-		
 		function _index($profile) {
 			
 			$this->log('Importing Modules for index.');
@@ -45,19 +60,25 @@
 			$this->_import();
 						
 			$this->log('Loading framework');
-			
-			$resume = false;
-			
-			//NEVER TRUST ANYTHING FROM THE USER!!!
-			if (ctype_alnum($this->params['continue']))
-				if (file_exists(TMP.$this->params['continue']))
-					if (($contents = file_get_contents(TMP.$this->params['continue']))!= '')
-						$resume = true;
-						
-			if ($resume) {
+									
+			if ($profile == true) {
 				
+				$this->log('And we are back where we left off');
+				
+				$contents = file_get_contents(TMP.$this->data['continue']);
+				$store = @unserialize($contents);
+				
+				if (!$store) {
+					$this->log('Error occurred while unserializing stored crawler, Contents:'.$contents);
+					$this->cakeError('error500');
+				}
+				
+				$indexer = $store['indexer'];
+				$crawler = $store['crawler'];
 			}
 			else {
+				
+				$this->log('Loading dependancies');
 			
 				$stopwords = $this->Stopword->find('all');
 				$elements = $this->Element->find('all');
@@ -87,20 +108,45 @@
 				$this->Index->save($save);
 								
 				if (get_time_left() <= 5)
-					$this->_shutdown($crawler);
+					$this->_shutdown($crawler, $indexer);
 					
 			}
 			
-			function _shutdown ($crawler) {
+			$this->log('Indexing Complete');
+			die();
+		}
+		function _shutdown ($crawler, $indexer) {
 			
+				$this->log('Preparing to Restart');
 				
 				$randstr = random_string(10);
-								
-				file_put_contents(TMP.$randstr, $crawler->save());
+
+				$save = serialize(array(
+					'crawler' => $crawler,
+					'indexer' => $indexer				
+				));
 				
-				//start_script($html->url(array('controller'=>'indexer', 'action'=>'index', true), true));
+				$this->log('Saving state');
+				
+				file_put_contents(TMP.$randstr, $save);
+				
+				$this->log('Restarting');
+				
+				start_script(
+					$html->url(
+						array(
+							'controller' => 'indexer',
+							'action' => 'start',
+							'admin' => true, 
+							'?' => array(
+								'data[continue]' => $randstr
+							)
+						),
+						true
+					)
+				);
 				
 				die();
 			}
-		}
+		
 	}
