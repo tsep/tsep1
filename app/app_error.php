5dadd418619d3b49ff13a73ff5d4b645e58a6b58
geoffreyfishing <>
2010-12-07 23:30:45 +0000

    Prepare for release

--- app/app_error.php
+++ app/app_error.php
@@ -1,6 +1,10 @@
 <?php 
 	class AppError extends ErrorHandler {
 		
+		function beforeFilter () {
+			$this->controller->set('title', 'An error has occurred');
+		}
+		
 		function error403 ($params) {
 			$this->_outputMessage('error_403');			
 		}
