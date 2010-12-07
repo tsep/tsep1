efed3132178b5a4cae1623cc56a17f2d59700d20
geoffreyfishing <>
2010-12-07 23:51:55 +0000

    Fixed installation security hole

--- app/plugins/install/install_app_controller.php
+++ app/plugins/install/install_app_controller.php
@@ -1,4 +1,11 @@
 <?php 
 	class InstallAppController extends AppController {
 		
+		function beforeFilter() {
+			parent::beforeFilter();
+			
+			if(file_exists(CONFIGS.'settings.ini.php')) {
+				$this->cakeError('error403');
+			}
+		}		
 	}
