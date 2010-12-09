7649f8936ac48a7014d2f0423045868c7b0e154c
geoffreyfishing <>
2010-12-09 15:30:13 +0000

    Fixed bug with installation.

--- app/plugins/install/install_app_controller.php
+++ app/plugins/install/install_app_controller.php
@@ -2,10 +2,12 @@
 	class InstallAppController extends AppController {
 		
 		function beforeFilter() {
-			parent::beforeFilter();
 			
 			if(file_exists(CONFIGS.'settings.ini.php')) {
 				$this->cakeError('error403');
+			}
+			else {
+				parent::beforeFilter(); //Calling this by default will cause errors
 			}
 		}		
 	}
