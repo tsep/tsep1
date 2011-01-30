3d089b4443cbd05013d74b3f107d30b63b40e331
geoffreyfishing@gmail.com <>
2011-01-30 23:32:18 +0000

    Added theme views.

---
+++ app/models/theme.php
@@ -0,0 +1,17 @@
+<?php
+/**
+* Theme Model
+* 
+* @author Geoffrey
+*
+* The following will be filled automatically by SubVersion!
+* Do not change by hand!
+*  $LastChangedDate: $
+*  $LastChangedBy:  $
+*  $LastChangedRevision: $
+*
+*/
+
+class Theme extends AppModel {
+	var $name = 'Theme';
+}
