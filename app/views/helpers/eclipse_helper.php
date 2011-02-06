bdf4b3dd1e3200cac1812bd295ca966e983ae77e
geoffreyfishing@gmail.com <>
2011-02-06 18:27:28 +0000

    Added code completion for eclipse.

---
+++ app/views/helpers/eclipse_helper.php
@@ -0,0 +1,28 @@
+<?php
+/**
+* Code Completion for Eclipse and other IDEs
+* 
+* @author Geoffrey
+*
+* The following will be filled automatically by SubVersion!
+* Do not change by hand!
+*  $LastChangedDate: $
+*  $LastChangedBy:  $
+*  $LastChangedRevision: $
+*
+*/
+
+exit(); 
+// auto-complete in views for Helpers 
+if(false) { 
+    $ajax = new AjaxHelper(); 
+    $cache = new CacheHelper(); 
+    $form = new FormHelper(); 
+    $html = new HtmlHelper(); 
+    $javascript = new JavascriptHelper(); 
+    $js = new JsHelper();
+    $number = new NumberHelper(); 
+    $session = new SessionHelper(); 
+    $text = new TextHelper(); 
+    $time = new TimeHelper(); 
+} 
