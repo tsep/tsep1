c045e16f0acaf941d56291d1544583b5d0ea86b4
geoffreyfishing@gmail.com <>
2011-02-08 00:11:42 +0000

    Prepare for release.

--- app/vendors/upgrade.php
+++ app/vendors/upgrade.php
@@ -21,4 +21,22 @@ function upgrade () {
 		
 
 	//TODO: Upgrade INI file
+	
+	$appController = new AppController();
+	
+	$ini = parse_ini_file(CONFIGS.'settings.ini.php', true);
+	
+	Configure::write('Database.driver', 'mysql');
+	Configure::write('Database.persistent', false);
+	Configure::write('Database.host', $ini['database']['host']);
+	Configure::write('Database.login', $ini['datbase']['login']);
+	Configure::write('Database.password', $ini['database']['password']);
+	Configure::write('Database.database', $ini['database']['gfishing_tests']);
+	Configure::write('Database.prefix', $ini['database']['prefix']);
+	
+	Configure::write('Security.salt', $ini['security']['salt']);
+	Configure::write('Security.cipherSeed', $ini['security']['chipherSeed']);
+	
+	$appController->_saveConfig();
+	
 }
