608a5f30b43ba3c0fdedd7398038a2819cb9806b
geoffreyfishing@gmail.com <>
2011-02-11 23:26:52 +0000

    Added the queue component.

---
+++ app/controllers/components/queue.php
@@ -0,0 +1,34 @@
+<?php
+/**
+* Queue Component
+* 
+* @author Geoffrey
+*
+* The following will be filled automatically by SubVersion!
+* Do not change by hand!
+*  $LastChangedDate: $
+*  $LastChangedBy:  $
+*  $LastChangedRevision: $
+*
+*/
+
+
+class QueueComponent extends Object {
+	
+	/**
+	 * Execute the next job in the queue
+	 */
+	function executeJob () {
+		
+		
+	}
+	
+	/**
+	 * Add a job to the queue
+	 * @param string $job_name The name of the job to add
+	 * @param string $job_options The parameters to be passed to the job
+	 */
+	function addJob($job_name, array $job_options) {
+		
+	}
+}
