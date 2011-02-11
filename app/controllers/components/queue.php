8d32935626ae2fccdab218448b1c68050d902c66
geoffreyfishing@gmail.com <>
2011-02-11 23:55:55 +0000

    Updated queue component.

--- app/controllers/components/queue.php
+++ app/controllers/components/queue.php
@@ -15,20 +15,52 @@
 
 class QueueComponent extends Object {
 	
+	
+	
 	/**
 	 * Execute the next job in the queue
 	 */
-	function executeJob () {
+	function executeJob ($job_id = null) {
 		
 		
+		
+		return $job_id;
 	}
 	
 	/**
 	 * Add a job to the queue
-	 * @param string $job_name The name of the job to add
+	 * @param string $job_name The name of the function to execute
 	 * @param string $job_options The parameters to be passed to the job
 	 */
 	function addJob($job_name, array $job_options) {
 		
+		
+		return $job_id;
+	}
+	
+	
+	/**
+	 * Removes the job with the given id
+	 * @param string $job_id The job to remove
+	 */
+	function removeJob ($job_id) {
+		
+		return $job_id;
+	}
+	
+	/**
+	 * Clear all jobs
+	 */
+	function clearAllJobs() {
+		
+		return true;	
+	}
+	
+	/**
+	 * Get the ID of the next job in the queue
+	 */
+	function nextJob() {
+		
+		return $job_id;
 	}
 }
