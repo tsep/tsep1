3081c8c006998c23d1cdc6d87d1c2a844053fd3a
geoffreyfishing@gmail.com <>
2011-02-17 02:02:55 +0000

    Added search suggestions framework.

---
+++ app/controllers/searches_controller.php
@@ -0,0 +1,18 @@
+<?php 
+/**
+* Searches Controller - Handles Search Suggestions
+* 
+* @author Geoffrey
+*
+* The following will be filled automatically by SubVersion!
+* Do not change by hand!
+*  $LastChangedDate: $
+*  $LastChangedBy:  $
+*  $LastChangedRevision: $
+*
+*/
+
+class SearchesController extends AppController {
+	
+	
+}
---
+++ app/models/search.php
@@ -0,0 +1,18 @@
+<?php 
+/**
+* Search Model
+* 
+* @author Geoffrey
+*
+* The following will be filled automatically by SubVersion!
+* Do not change by hand!
+*  $LastChangedDate: $
+*  $LastChangedBy:  $
+*  $LastChangedRevision: $
+*
+*/
+
+class Search extends AppModel {
+	
+	var $name = 'Search';
+}
