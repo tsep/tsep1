4dea85a9504f3562c53d54b61d9e3de67f10c8b9
geoffreyfishing@gmail.com <>
2011-02-17 02:10:47 +0000

    Defined methods.

--- app/models/search.php
+++ app/models/search.php
@@ -15,4 +15,20 @@
 class Search extends AppModel {
 	
 	var $name = 'Search';
+	
+	/**
+	 * Get all the searches that match $phrase
+	 * @param string $phrase The phrase to look for
+	 */
+	function get($phrase) {
+		
+	}
+	
+	/**
+	 * Adds a search $phrase to the log
+	 * @param string $phrase The phrase to add
+	 */
+	function add($phrase) {
+		
+	}
 }
